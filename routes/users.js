de5afa0bcedf97c4c18ae27fc55d2315174dcd2f
wangpossible <wsffirst@gmail.com>
2015-03-03 19:39:20 +0800

    add mongoose support

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,140 @@
 var express = require('express');
 var router = express.Router();
+var mongoose=require('mongoose');
+var hash=require('../pass').hash;
+
+mongoose.connect('mongodb://localhost:/myapp');
+var userSchema=new mongoose.Schema({
+	username:String,
+	password:String,
+	salt:String,
+	hash:String
+});
+var User=mongoose.model('users',userSchema);
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  if(req.session.user){
+	  res.send('welcome ' + req.session.user.username + '<a href="/users/logout">logout</a>');
+  }else{
+	  res.send('<a href="/users/login">Login</a><a href="/users/signup">Sign Up</a>');
+  }
 });
+
+router.get('/signup', function(req, res) {
+	if(req.session.user){
+		res.redirect('/');
+	}else{
+		res.render('signup');
+	}
+});
+
+router.get('/login', function(req, res) {
+	if(req.session.user){
+		res.redirect('/');
+	}else{
+		res.render('login');
+	}
+});
+
+router.get('/logout', function (req, res) {
+    req.session.destroy(function () {
+        res.redirect('/');
+    });
+});
+
+router.get('/prifile',requiredAuthentication, function (req, res) {
+    res.send('Profile page of '+ req.session.user.username +'<br>'+' click to <a href="/logout">logout</a>');
+});
+
+router.post('/sinup',isUserExisted,function (req, res) {
+	console.log(req);
+	var usrname=req.body.username;
+	var password=req.body.password;
+	console.log(req.body.username);
+	console.log(req.body.password);
+	hash(password,function(err,salt,hash){
+		if(err) throw err;
+		var user=new User({
+			username:username,
+			salt:salt,
+			hash:hash
+		}).save(function(err,newUser){
+			if(err) throw err;
+			authenticate(newUser.username,password,function(err,user){
+				if(user){
+						req.session.regenerate(function(){
+								req.session.user=user;
+								req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>.';
+								res.redirect('/');
+						});
+				}
+			});
+		});
+	});
+});
+
+router.post('/login',function(req,res){
+	console.log(req.body.username);
+	console.log(req.body.password);
+	authenticate(req.body.username, req.body.password, function (err, user) {
+        if (user) {
+            req.session.regenerate(function () {
+                req.session.user = user;
+                req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ';
+                res.redirect('/');
+            });
+        } else {
+            req.session.error = 'Authentication failed, please check your ' + ' username and password.';
+            res.redirect('/users/login');
+        }
+    });
+	
+});
+
+
+/* authenticate current user*/
+function authenticate(username,password,fn){
+	console.log('authenticating %s:%s',username,password);
+	User.findOne({
+		username:username
+	},function(err,user){
+		if(user){
+				if(err) return fn(new Error('connot find user'));
+				hash(password,user.salt,function(err,hash){
+					if(err) return fn(err);
+					if(hash == user.hash) return fn(null,user);		
+					fn(new Error('invalid password'));
+				});
+		}else{
+			return fn(new Error('connot find user'));
+		}
+	});
+}
+
+/*current session whether contains user*/
+function requiredAuthentication(req,res,next){
+	if(req.session.user){
+		next();
+	}else{
+		req.session.error = 'Access denied!';
+		res.redirect('/login');
+	}	
+}
+
+/*when regist need check whether the user  has existed */
+function isUserExisted(req,res,next){
+		User.count({
+			username:req.body.username
+		},function(err,count){
+			if(count==0){
+				next();
+			}else{
+				req.session.error('user exist');
+				res.redirect('/sinup');
+			}	
+		});
+	
+}
 
 module.exports = router;
