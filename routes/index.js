de4d7d2d66003516344fbe2e1da3d9db99f4e37f
wangshoufang <wangpossible.fang@gmail.com>
2015-07-16 10:11:16 +0800

    add google check

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: '楚汉单车俱乐部',active:'active' });
 });
 
+/* GET home page. */
+router.get('/googlec2593dadb6786fe1.html', function(req, res) {
+  res.render('google', { title: '楚汉单车俱乐部',active:'active' });
+});
+
 router.get('/product', function(req, res) {
   res.render('product', { title: '装备库',active:'active' });
 });
