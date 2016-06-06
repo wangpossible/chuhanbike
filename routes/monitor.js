b596e5560d1de3125777574749fb7f57c9e8c8b5
wangshoufang <wangshoufang@jd.com>
2016-06-06 13:09:06 +0800

    monitor

---
+++ routes/monitor.js
@@ -0,0 +1,64 @@
+var schedule = require('node-schedule');
+var superagent = require('superagent');
+var nodemailer=require('nodemailer');
+
+
+var transporter=nodemailer.createTransport({
+  service:'163',
+  auth:{
+       user:'monitorproject@163.com',
+	     pass:'bullshit123456'
+  }
+});
+
+var mailOptions={
+  from:'monitorproject@163.com',
+  to:'18511695865@163.com,jingpinhui_baojing@foxmail.com',
+  subject:'',
+  text:'',
+  html:''
+};
+
+
+/* every minute */
+var job = schedule.scheduleJob('*/1 * * * *', function(){
+  console.log('The answer to life, the universe, and everything!');
+  requestTuan();
+});
+
+function requestTuan(){  
+    var url='http://tuan.jd.com/beijing-index.html';
+	superagent.get(url).end(function(err,res){
+		if (err) {
+			mailOptions.subject="首页访问异常";
+			mailOptions.text="首页访问异常test";
+			mailOptions.html="首页访问异常test";
+  			sendMail(mailOptions);
+		}else{
+			var status =res.status;
+			if(status!=200){
+				mailOptions.subject="首页访问异常";
+				mailOptions.text="首页访问异常,status："+status;
+				mailOptions.html="首页访问异常,status："+status;
+				sendMail(mailOptions);
+			}else{
+				mailOptions.to="18511695865@163.com";
+				mailOptions.subject="首页访问正常";
+				mailOptions.text="首页访问正常,status："+status;
+				mailOptions.html="首页访问正常,status："+status;
+				sendMail(mailOptions);
+			}
+		}
+	}); 
+}
+
+
+function sendMail(options){
+	transporter.sendMail(options,function(error,info){
+		if(error){
+			console.log(error);
+		}else{
+			console.log('Message send'+info.response);
+		}
+	});	
+}
